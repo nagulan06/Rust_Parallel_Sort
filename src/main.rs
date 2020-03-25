48b7d7d198627e72d931da726494efbc5b42a6ce
nagulan <manivelan.n@husky.neu.edu>
2020-03-24 21:44:03 -0400

    parallel sorting and file handling. DONE

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,7 @@ use std::f32;
 use std::thread;
 use std::sync::{Arc, Barrier, Mutex};
 use std::convert::TryInto;
-/*
-use rand::Rng;
-use std::env;
-use std::fs::{File};
-use std::io::{Read, Cursor, Seek, SeekFrom};
-*/
+
 extern crate bytes;
 use bytes::Buf;
 
@@ -38,13 +33,11 @@ fn main() {
     let pivots = find_pivots(&mut inpf, threads, size);
     
     // Create output file
-    {
-        let mut outf = File::create(out_path).unwrap();
-        let tmp = size.to_ne_bytes();
-        outf.write_all(&tmp).unwrap();
-        outf.set_len(size*4 + 8).unwrap();
-    }
-
+    let mut outf = File::create(out_path).unwrap();
+    let tmp = size.to_ne_bytes();
+    outf.write_all(&tmp).unwrap();
+    outf.set_len(size).unwrap();
+    
     let mut workers = vec![];
 
     // Spawn worker threads
@@ -70,16 +63,16 @@ fn main() {
     }
     
 }
-
+// Read size field from data file and return it
 fn read_size(file: &mut File) -> u64 {
-    // TODO: Read size field from data file
     let mut count = [0u8; 8];
     file.read_exact(&mut count).unwrap();
     let xx = Cursor::new(count).get_u64_le();
     xx
 }
 
-fn read_item(file: &mut File, ii: u64) -> f32 {
+//read the iith item from the file and return it
+fn read_item(file: &mut File, ii: u64) -> f32 { 
     let mut tmp = [0u8;4];
     file.seek(SeekFrom::Start(8 + ii*4)).unwrap();
     file.read_exact(&mut tmp).unwrap();
@@ -87,11 +80,11 @@ fn read_item(file: &mut File, ii: u64) -> f32 {
     xx
 }
 
+//sample "count" random items from the file and return the vector that contains the sample
 fn sample(file: &mut File, count: usize, size: u64) -> Vec<f32> {
     let mut rng = rand::thread_rng();
     let mut ys = vec![];
     let mut index = 0;
-    //sample "count" random items from the file
     while index < count{
         let rand_val = rng.gen_range(0, size);
         ys.push(read_item(file, rand_val));
@@ -101,17 +94,16 @@ fn sample(file: &mut File, count: usize, size: u64) -> Vec<f32> {
     ys
 }
 
+//Sort the sampled list, find medians, fill out the pivots array and return it
 fn find_pivots(mut file: &mut File, threads: usize, size: u64) -> Vec<f32> {
     let mut pivots = vec![0f32; threads+1];
     const INFINITY:f32 = 1.0f32/0.0f32;
     //Sample 3*(threads-1) items from the file
     let count = 3*(threads - 1);
     let mut samples = sample(&mut file, count, size);
-   // println!("the sampled values = {:?}\n", samples);
     
     // Sort the sampled list
     samples.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    //println!("the sorted values = {:?}\n", samples);
     // push the pivots into the array
     let mut index = 1;
     let mut i = 0;
@@ -126,6 +118,7 @@ fn find_pivots(mut file: &mut File, threads: usize, size: u64) -> Vec<f32> {
     pivots
 }
 
+//Each thread calls this function, which finds data that belong to its range ,sorts them parallely and puts them onto the output file at the correct location.
 fn worker(tid: usize, inp_path: String, out_path: String, pivots: Vec<f32>,
           sizes: Arc<Mutex<Vec<u64>>>, bb: Arc<Barrier>) {
 
@@ -134,7 +127,7 @@ fn worker(tid: usize, inp_path: String, out_path: String, pivots: Vec<f32>,
     let size = read_size(&mut inpf);
 
     //Scan to collect local data
-    let mut data = vec![];
+    let mut data: Vec<f32> = Vec::new();
     for i in 0..size
     {
         let item = read_item(&mut inpf, i);
@@ -143,33 +136,26 @@ fn worker(tid: usize, inp_path: String, out_path: String, pivots: Vec<f32>,
             data.push(item);
         }
     }
-    //Write local size to shared sizes
 
+    //Write local size to shared sizes
+    //since sizes is a shared array, it requires a lock to access it
+    // curly braces to scope our lock guard
     {
         let mut sizes = sizes.lock().unwrap();
         sizes[tid] = data.len().try_into().unwrap();
-        //println!("sizes[{}] = {}", tid, sizes[tid]);
-        // curly braces to scope our lock guard
     }
 
     // Sort local data
     data.sort_by(|a, b| a.partial_cmp(b).unwrap());
 
-    // Write data to local buffer
-    let mut cur = Cursor::new(vec![]);
-    for xx in &data {
-        let tmp = xx.to_bits().to_ne_bytes();
-        cur.write_all(&tmp).unwrap();
-    }
-    
     // calculate the start and end index for the current thread
+    //But this requires all the thread to have completed calculating its size. Hence, barrier wait is used.
 
     bb.wait();
     let mut start = 0;
     //Get position for output file
-
     let prev_count = {
-        // curly braces to scope our lock guard
+        //cope of the lock guard is within this "let" block.
         let sizes = sizes.lock().unwrap();
             for i in 0..tid
             {
@@ -180,6 +166,7 @@ fn worker(tid: usize, inp_path: String, out_path: String, pivots: Vec<f32>,
 
 
     // Here's our printout
+    // The if condition is to make sure that we don't access the data index when it doesn't exist and hence leading to program crashing
     if data.len() != 0 
     {
         println!("{}: start {}, count {}", tid, &data[0], data.len());
@@ -189,20 +176,21 @@ fn worker(tid: usize, inp_path: String, out_path: String, pivots: Vec<f32>,
         println!("{}: count {}", tid, data.len());
     }
 
-
+    //open file with read and write permissions
     let mut outf = OpenOptions::new()
         .read(true)
         .write(true)
         .open(out_path).unwrap();
     
     //Seek and write local buffer.
-    println!("{}: data = {:?}", tid, data);
-    outf.seek(SeekFrom::Start(8 + prev_count)).unwrap();
-    /*
+    //Seek to the right position where the current thread has to put data into the output file
+    outf.seek(SeekFrom::Start(8 + prev_count*4)).unwrap();
+    
+    //Put all the data into the file.
     for i in 0..data.len()
     {
         let tmp = data[i].to_bits().to_ne_bytes();
         outf.write_all(&tmp).unwrap();
     }
-    */
+    
 }
